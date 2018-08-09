e77ef6a9ef72bde183712ee23d41be81de1d1fe6
Sergio Galindo Mundo <sgalindo.smartsoft@gmail.com>
2018-08-09 17:42:06 -0500

    Se agrego la libreria excel para los reportes y la libreria view para renderirizar las vistas

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,26 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use View;
 class ClienteController extends Controller
 {
    
-      public function index(){                        
-      return view('cliente/listClientes');
-
+      public function index(){      
+          
+           return View::make('cliente.listClientes')->render();             
     }
     public function getClientes(){
         
     }
+    
     public function  addClienteGet(){
-          return view('cliente/addCliente');
-
+     return   View::make('cliente.addCliente')->render(); 
+         
     }
     public function deleteCliente(){
         
     }
     public function updateCliente(){
-        
+            return View::make('cliente.updateCliente')->render();          
     }
-    
+       private function exportFile($data){
+        ob_end_clean();
+        ob_start();
+        Excel::create('Order_report_'.date('Y_m_d-H:i:s'), function ($excel) use ($data) {
+            $excel->sheet('Order', function ($sheet) use ($data) {
+                $sheet->with($data, null, 'A1', false, false);
+            });
+        })->download('xlsx');
+    }
+
 }
--- app/Http/Controllers/DeporteController.php
+++ app/Http/Controllers/DeporteController.php
@@ -3,34 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use View;
 class DeporteController extends Controller
 {
     
-    public function getDeportes(){
-        
-    }
+  
      public function index(){
-        return view('deporte/listDeportes');
+         return View::make('deporte.listDeportes')->render();             
+       
     }
     public function addDeporte(){
+         return View::make('deporte.addDeporte')->render(); 
+    }
+ 
+    public function updateDeporte(){
+                 return View::make('deporte.updateDeporte')->render(); 
+
+    }
+    
+    //post functions
+    
+    public function deleteDeporte(){
         
     }
-    public function saveDeporte(){
+       public function saveDeporte(){
         
     }
     
     public function getDeporte(){
         
     }
-    public function updateDeporte(){
-        
-    }
-    
-    public function deleteDeporte(){
-        
-    }
-    
     
     
 }
